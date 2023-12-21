2b71646f8e02f5c2536a39067610b3372357fd72
Ravsst2004 <ravindraviswanatha@gmail.com>
2023-12-21 14:17:11 +0800

    Change diretory

--- View/index.php
+++ View/index.php
@@ -1,9 +1,9 @@
 <?php 
-    require_once './View/template/header.php';
+    require_once './template/header.php';
 ?>
 
 <h1>LOL</h1>
 
 <?php 
-    require_once './View/template/footer.php';
+    require_once './template/footer.php';
 ?>
