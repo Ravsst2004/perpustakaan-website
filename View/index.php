31350ec19e0adeb9c7b36e42802ec5e6d75802e8
Ravsst2004 <ravindraviswanatha@gmail.com>
2023-12-21 14:07:42 +0800

    Change directory

--- View/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    require_once 'template/header.php'
-?>
-
-<h1>LOL</h1>
-
-<?php 
-    require_once 'template/footer.php'
-?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+    require_once './View/template/header.php';
+?>
+
+<h1>LOL</h1>
+
+<?php 
+    require_once './View/template/footer.php';
+?>
